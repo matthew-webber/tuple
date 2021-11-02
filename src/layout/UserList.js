5b0d7059c09e5e43da88458766bf41091b6abaef
Matt Webber <mattwebbersemail@gmail.com>
2021-11-02 11:46:55 -0400

    Update UserList.js

--- src/layout/UserList.js
+++ src/layout/UserList.js
@@ -6,13 +6,8 @@ const UserList = ({users, usersType, editing, x}) => {
   return (
     <>
       {users.map((user, i) => {
-        console.log(i)
-        console.log(users.length)
-        const last = i === users.length - 1 && true
-
         return (
           <UserCard
-            last={last ? x : ''}
             userData={user}
             friend={usersType === 'friends' && true}
           >
